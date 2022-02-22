1db41bc0ebbfcaba0b7629febf852c24b83c0bec
Mohsen9april <Mohsen9april@gmail.com>
2022-02-22 22:28:01 +0330

    add imageslider & H-scroll

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  loop:true,
+
+});
